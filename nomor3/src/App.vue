f613c38d255f1e1825d5ded1d9ced75e953a219f
Anton Prafanto <anton.prafanto@gmail.com>
2023-11-11 22:49:27 +0800

    feat: nomor 3 selesai

---
+++ nomor3/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+  <h1>Nilai 1 sampai 50</h1>
+  <ul>
+    <li v-for="number in numbers" :key="number"> {{ number }}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      numbers: Array.from({length: 50}, (_,i) => i + 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul,li{
+  list-style-type: none;
+}
+</style>
